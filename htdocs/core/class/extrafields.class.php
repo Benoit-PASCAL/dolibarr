959de219ef22759f8b7a38e3c5d738a3ff3c3a6c
Benoît PASCAL <pcl.ben.6@gmail.com>
2024-10-28 08:45:30 +0100

    style: run cs fixer

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -1292,7 +1292,7 @@ class ExtraFields
 			$out = '<input type="text" class="flat '.$morecss.' maxwidthonsmartphone" name="'.$keyprefix.$key.$keysuffix.'" id="'.$keyprefix.$key.$keysuffix.'" value="'.$value.'" '.($moreparam ? $moreparam : '').'> ';
 			$out .= $form->selectCurrency($currency, $keyprefix.$key.$keysuffix.'currency_id');
 		} elseif ($type == 'duration') {
-			$out = $form->select_duration($keyprefix . $key,$value, 0, 'text', '', 1);
+			$out = $form->select_duration($keyprefix . $key, $value, 0, 'text', '', 1);
 		} elseif ($type == 'double') {
 			if (!empty($value)) {		// $value in memory is a php numeric, we format it into user number format.
 				$value = price($value);
